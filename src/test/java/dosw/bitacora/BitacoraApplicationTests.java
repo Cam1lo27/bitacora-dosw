596051d90c6039cd62e0b13fb8cf1d5756499fc5
Andres Vivas <andres.vivas-b@mail.escuelaing.edu.co>
2026-02-02 01:10:25 -0500

    Inicialización del proyecto Maven Bitácora DOSW

---
+++ src/test/java/dosw/bitacora/BitacoraApplicationTests.java
@@ -0,0 +1,13 @@
+package dosw.bitacora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitacoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
